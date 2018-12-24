79acb9f652ea774a6511ee2adc8706990b881d85
Roman A Voiers <41023358+romanvoiers@users.noreply.github.com>
2018-12-24 11:16:00 -0600

    First Commit
    First commit of a really simple calculator

---
+++ BuildingACalc.py
@@ -0,0 +1,7 @@
+num1 = input("What is the first number? ")
+num2 = input("What is the second number? ")
+result = int(num1) + int(num2)
+print(result)
+
+# By default python takes input as a string, you have to convert them into numbers using int.
+# or use float for decimal numbers
