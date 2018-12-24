f651fb084359fa9ebbbe3aae01d958429c7f771b
Roman A Voiers <41023358+romanvoiers@users.noreply.github.com>
2018-12-24 11:14:20 -0600

    First Upload
    First commit of a MadLibsGame

---
+++ MadLibsGame.py
@@ -0,0 +1,8 @@
+color = input("Enter a color ")
+plural_noun = input("Enter a plural noun ")
+celbrity = input("Enter a name of a celbrity ")
+
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I hate " + celbrity)
+
