c35e1194a280fde6db0be53e7161039255788413
Roman A Voiers <41023358+romanvoiers@users.noreply.github.com>
2018-12-24 11:04:27 -0600

    Initial Upload
    First upload with Print commands

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,10 @@
-print("Hello world")
+character_name = "Roman"
+
+print("Hello \nWorld")  # \n puts what ever after it on a new line.
+print("Hello " + character_name)
+print(character_name.upper(). isupper())  # Makes the var(character_name) upper case.
+print(len(character_name))  # counts how many letters in the string
+print((character_name[0]))  # grabs the first letter of the string
+print(character_name.index("m"))
+print(character_name.replace("Roman", "Risky"))  # Replaces the old variable string with a new variable string
+
