349b71d621a7320e0afb13355b4fc753d6a30e53
Roman A Voiers <41023358+romanvoiers@users.noreply.github.com>
2018-12-24 11:13:04 -0600

    First Commit
    First Upload of getting user input

---
+++ GettingInputUsers.py
@@ -0,0 +1,7 @@
+name = input("What is your name User? ")  # Taking the value from user input and storing it into name Var.
+age = input("How old are you? ")
+
+print("Welcome " + name + "!!")
+print("You are " + age + " years old!!")
+
+
