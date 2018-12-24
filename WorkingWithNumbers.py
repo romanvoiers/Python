77089fd69b1a9b321944e60caf127979fc46a0c4
Roman A Voiers <41023358+romanvoiers@users.noreply.github.com>
2018-12-24 11:11:14 -0600

    First Commit
    first Upload of working with numbers in python

---
+++ WorkingWithNumbers.py
@@ -0,0 +1,38 @@
+from math import *  # imports more math functions. Also called a module
+
+
+my_num = 27
+my_abs_num = -10
+
+print(1, 2, 3, 4)
+print(5 + 5)  # Don't need variables to do math
+print(10 * (5 + 5))
+print(10 * 5 + 5)  # Order of operations
+print(10 % 3)  # Spits out the remainder
+print(my_num)  # Prints out the variable
+print(str(my_num))  # Converts number into a string
+print(str(my_num) + " is my Favorite number.")  # in order to add strings to a number you need to convert
+print(abs(my_abs_num))  # Gives the absolute value
+print(pow(5, 2))  # Pow takes two values. First: num Second: Power of
+print(max(5, 10))  # Max outputs largest number
+print(min(5, 10))  # Min outputs lowest number
+print(round(3.2))  # Rounds the number
+print(floor(3.945))  # outputs base number
+print(ceil(3.2))  # Always rounds up
+print(sqrt(36))  # Squares the number
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
