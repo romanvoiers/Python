b25758cb76f702fb402a9e897e876b1b05330c38
Roman A Voiers <41023358+romanvoiers@users.noreply.github.com>
2018-12-26 19:04:53 -0600

    Now shows the whole final equation

--- BuildingACalc.py
+++ BuildingACalc.py
@@ -1,19 +1,27 @@
 num1 = input("What is the first number? ")
 num2 = input("What is the second number? ")
-operation = input("What mathematical operation you will be using? +,-,/ or *?: ")
+operation = input("What mathematical operation you will be using? +,-,/,*,** ?: ")
 
 if operation == "+":
     result = int(num1) + int(num2)
-    print(result)
+    print(str(num1) + " + " + (str(num2) + " = " + (str(result))))
 
 if operation == "-":
     result = int(num1) - int(num2)
-    print(result)
+    print(str(num1) + " - " + (str(num2) + " = " + (str(result))))
 
 if operation == "/":
-    result = float(num1) / float(num2)
-    print(result)
+    result = int(num1) / int(num2)
+    print(str(num1) + " / " + (str(num2) + " = " + (str(result))))
 
 if operation == "*":
     result = int(num1) * int(num2)
-    print(result)
+    print(str(num1) + " * " + (str(num2) + " = " + (str(result))))
+
+if operation == "**":
+    result = int(num1) ** int(num2)
+    print(str(num1) + " ** " + (str(num2) + "= " + (str(result))))
+
+
+# By default python takes input as a string, you have to convert them into numbers using int.
+# or use float for decimal numbers
