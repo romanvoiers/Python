bcbac47e929ecd2a34282149bcb69d78da653e09
Roman A Voiers <41023358+romanvoiers@users.noreply.github.com>
2018-12-28 17:03:49 -0800

    Better Flow
    Now the program asks the user for the first number then the operation then the second number

--- BuildingACalc.py
+++ BuildingACalc.py
@@ -1,6 +1,6 @@
 num1 = input("What is the first number? ")
-num2 = input("What is the second number? ")
 operation = input("What mathematical operation you will be using? +,-,/,*,** ?: ")
+num2 = input("What is the second number? ")
 
 if operation == "+":
     result = int(num1) + int(num2)
@@ -23,5 +23,5 @@ if operation == "**":
     print(str(num1) + " ** " + (str(num2) + "= " + (str(result))))
 
 
-# By default python takes input as a string, you have to convert them into numbers using int.
-# or use float for decimal numbers
+
+
