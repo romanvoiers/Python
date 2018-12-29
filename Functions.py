7d6f95aaf87a6f7a86844950e60c266938714028
Roman A Voiers <41023358+romanvoiers@users.noreply.github.com>
2018-12-28 19:06:23 -0600

    Initial Commit
    Shows the basics of a Function and returning Parameters

---
+++ Functions.py
@@ -0,0 +1,8 @@
+
+def say_hello(name, last_name):  # Makes a function with parameter
+    print("Whats up " + name + last_name)
+
+
+print("Top")
+say_hello("Risky", " Crimson")  # Calls the function adds the parameter
+print("Bottom")
